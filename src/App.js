3ceba4b019358710df13a15665087a5d72534cab
michaelpramastama <michaelwidap@gmail.com>
2020-04-26 15:40:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div>
-      <p>You clicked {count} times</p>
+      <p>You  {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
