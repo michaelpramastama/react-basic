9c9a31fff914f20c1d92c9ef7966cb1e74fdcc2f
michaelpramastama <michaelwidap@gmail.com>
2020-04-28 02:30:02 +0700

    Update contact.js

--- src/containers/layout/pages/contact.js
+++ src/containers/layout/pages/contact.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { TimePicker } from 'antd';
+import moment from 'moment';
+
+function onChange(time, timeString) {
+    console.log(time, timeString);
+}
 
 export default class contact extends Component {
     render() {
         return (
             <div>
                 <h1>Contact</h1>
+                <TimePicker onChange={onChange} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
             </div>
         );
     }
