a232a291c63d9a6a4a0dafcdf33b6ccf04f3ec0a
michaelpramastama <michaelwidap@gmail.com>
2020-04-27 19:32:48 +0700

    change button trigger

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Button } from 'antd';
+// import { Button } from 'antd';
 import Dasboard from './containers/layout/dasboard/index';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
