6aded44abd2e73c73dfbc1ffd46dd30512520545
michaelpramastama <michaelwidap@gmail.com>
2020-04-27 13:41:52 +0700

    change layout

--- src/containers/layout/dasboard/index.js
+++ src/containers/layout/dasboard/index.js
@@ -1,61 +1,160 @@
 import React from 'react';
 import { Layout, Menu, Icon } from 'antd';
 
-const { Header, Sider, Content } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
-class SiderDemo extends React.Component {
-    state = {
-        collapsed: false,
-    };
+const Dasboard = () => {
+return(
+    <Layout>
+        <Sider
+            style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0,
+            }}
+        >
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                <Menu.Item key="1">
+                    <Icon type="user" />
+                    <span className="nav-text">nav 1</span>
+                </Menu.Item>
+                <Menu.Item key="2">
+                    <Icon type="video-camera" />
+                    <span className="nav-text">nav 2</span>
+                </Menu.Item>
+                <Menu.Item key="3">
+                    <Icon type="upload" />
+                    <span className="nav-text">nav 3</span>
+                </Menu.Item>
+                <Menu.Item key="4">
+                    <Icon type="bar-chart" />
+                    <span className="nav-text">nav 4</span>
+                </Menu.Item>
+                <Menu.Item key="5">
+                    <Icon type="cloud-o" />
+                    <span className="nav-text">nav 5</span>
+                </Menu.Item>
+                <Menu.Item key="6">
+                    <Icon type="appstore-o" />
+                    <span className="nav-text">nav 6</span>
+                </Menu.Item>
+                <Menu.Item key="7">
+                    <Icon type="team" />
+                    <span className="nav-text">nav 7</span>
+                </Menu.Item>
+                <Menu.Item key="8">
+                    <Icon type="shop" />
+                    <span className="nav-text">nav 8</span>
+                </Menu.Item>
+            </Menu>
+        </Sider>
+        <Layout style={{ marginLeft: 200 }}>
+            <Header style={{ background: '#fff', padding: 0 }} />
+            <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                    ...
+          <br />
+          Really
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          long
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          content
+        </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+    </Layout>
+)
+};
 
-    toggle = () => {
-        this.setState({
-            collapsed: !this.state.collapsed,
-        });
-    };
-
-    render() {
-        return (
-            <Layout>
-                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">
-                            <Icon type="user" />
-                            <span>nav 1</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="video-camera" />
-                            <span>nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="upload" />
-                            <span>nav 3</span>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }}>
-                        <Icon
-                            className="trigger"
-                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                            onClick={this.toggle}
-                        />
-                    </Header>
-                    <Content
-                        style={{
-                            margin: '24px 16px',
-                            padding: 24,
-                            background: '#fff',
-                            minHeight: 280,
-                        }}
-                    >
-                        Content
-          </Content>
-                </Layout>
-            </Layout>
-        );
-    }
-}
-
-ReactDOM.render(<SiderDemo />);
+export default Dasboard;
